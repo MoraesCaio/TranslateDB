20b7878dadcc238a8593ce2a6711b22b55ba7e0c
MoraesCaio <caiomoraes.cesar@gmail.com>
2019-02-21 13:18:57 +0000

    Pool de Thread

--- translate_db.py
+++ translate_db.py
@@ -1,44 +1,82 @@
+import concurrent.futures
 import os
 import subprocess
 import sys
+import multiprocessing
 
 
-def translate(file_path):
-    print('>>> TRANSLATING...')
-    result = subprocess.run(['python', 'TraduzirArquivo.py', file_path])
-    print('>>> TRANSLATION FINISHED.')
+core_count = multiprocessing.cpu_count()
+
+
+def translate(dirpath, filename):
+
+    glosa_filename = filename[:-4] + '_glosa.txt'
+
+    source_dir = os.path.join(dirpath, 'source')
+    glosa_dir = os.path.join(dirpath, 'glosa')
+
+    # PATHS
+    source_path_from = os.path.join(dirpath, filename)
+    source_path_to = os.path.join(source_dir, filename)
+    glosa_path_from = os.path.join(dirpath, glosa_filename)
+    glosa_path_to = os.path.join(glosa_dir, glosa_filename)
+
+    # TRANSLATION
+    print('>>> TRANSLATING "' + source_path_from + '"...')
+    result = subprocess.run(['python', 'TraduzirArquivo.py', source_path_from])
+    print('>>> ' + source_path_from + ' TRANSLATED.')
+
+    # FILES
+    print('>>> MOVING "' + source_path_from + '" -> "' + source_path_to + '"')
+    os.rename(source_path_from, source_path_to)
+    print('>>> MOVING "' + glosa_path_from + '" -> "' + glosa_path_to + '"')
+    os.rename(glosa_path_from, glosa_path_to)
+
+    print('>>> FINISHED.')
+
     return result
 
 
-def translate_on_path(path, command):
+def translate_on_path(path):
     dirpath, _, filenames = next(os.walk(path))
-    
-    source_path = os.path.join(dirpath, 'source')
-    glosa_path = os.path.join(dirpath, 'glosa')
-    
-    if not os.path.exists(source_path):
-        os.makedirs(source_path)
-    if not os.path.exists(glosa_path):
-        os.makedirs(glosa_path)
 
-    for filename in filenames:
-        glosa_filename = filename[:-4]+'_glosa.txt'
-        
-        print('>>> READING FILE:', os.path.join(dirpath, filename))
-        out = translate(os.path.join(dirpath, filename))
-        
-        print('>>> MOVING FILES:')
-        os.rename(os.path.join(dirpath, filename),
-                  os.path.join(source_path, filename))
+    source_dir = os.path.join(dirpath, 'source')
+    glosa_dir = os.path.join(dirpath, 'glosa')
 
-        os.rename(os.path.join(dirpath, glosa_filename),
-                  os.path.join(glosa_path, glosa_filename))
-        print('>>> FINISHED.')
+    if not os.path.exists(source_dir):
+        os.makedirs(source_dir)
+    if not os.path.exists(glosa_dir):
+        os.makedirs(glosa_dir)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=core_count) as e:
+
+        future_translation = {e.submit(translate, dirpath, filename): filename
+                              for filename in filenames}
+
+        for future in concurrent.futures.as_completed(future_translation):
+
+            filename = future_translation[future]
+
+            try:
+
+                data = future.result()
+
+            except Exception as exc:
+
+                print('%r generated an exception: %s' %
+                      (os.path.join(dirpath, filename), exc))
+
+            else:
+
+                print('>>> ENDING THREAD FOR "' + filename +
+                      '" TRANSLATION. RESULT: ' + str(data.returncode))
+
+    print('>>> DB "' + path + '" TRANSLATED')
 
 
 def main():
     for i in range(1, len(sys.argv)):
-        translate_on_path(sys.argv[i], translate)
+        translate_on_path(sys.argv[i])
 
 
 if __name__ == '__main__':
